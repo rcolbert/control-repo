96ebc612022f7eb1652972fff70857ab2fbd4443
rcolbert <r.p.colbert@gmail.com>
2020-05-19 00:41:32 +0000

    updating node info

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,17 @@
+node puppetmaster.mantech.com {
+  include role::master
+  file { '/etc/secret_password.txt':
+    ensure => file,
+    content => lookup('secret_password'),
+  }
+}
 node puppetagent1.mantech.com {
   include role::master
   file { '/etc/secret_password.txt':
     ensure => file,
     content => lookup('secret_password'),
   }
+}
+node default {
+  include role::master
 }
