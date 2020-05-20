e06c97b5f51181737b87f53d5600705dcf3ef86b
root <root@ip-172-31-36-189.ec2.internal>
2020-05-20 23:23:36 +0000

    adding node 2

--- manifests/site.pp
+++ manifests/site.pp
@@ -12,6 +12,13 @@ node puppetagent1.mantech.com {
     content => lookup('secret_password'),
   }
 }
+node puppetagent2.mantech.com {
+  include role::master
+#  file { '/etc/secret_password.txt':
+#    ensure => file,
+#    content => lookup('secret_password'),
+#  }
+}
 node default {
   include role::master
 }
